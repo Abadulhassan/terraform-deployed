1c2b308d67da7131738429438cc4b7db581df454
Abadulhassan <45556780+Abadulhassan@users.noreply.github.com>
2026-02-10 17:19:45 +0500

    Update main.tf
    3 tier architecture deployment

--- main.tf
+++ main.tf
@@ -2,23 +2,103 @@ provider "aws" {
   region = "us-west-2"
 }
 
+############################
+# VPC + SUBNETS
+############################
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "private1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+}
+
+############################
+# SECURITY GROUPS
+############################
+
+resource "aws_security_group" "lambda_sg" {
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lambda_sg.id]
+  }
+}
+
+############################
+# IAM ROLE FOR LAMBDA
+############################
+
 resource "aws_iam_role" "lambda_role" {
-  name = "jenkins_lambda_role"
+  name = "lambda-rds-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
       Principal = { Service = "lambda.amazonaws.com" }
     }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_basic" {
+resource "aws_iam_role_policy_attachment" "basic" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_iam_role_policy_attachment" "vpc" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+############################
+# RDS DATABASE
+############################
+
+resource "aws_db_subnet_group" "rds_subnets" {
+  subnet_ids = [aws_subnet.private1.id, aws_subnet.private2.id]
+}
+
+resource "aws_db_instance" "app_db" {
+  identifier             = "jenkins-app-db"
+  engine                 = "mysql"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  username               = "admin"
+  password               = "Admin12345!"
+  db_name                = "usersdb"
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnets.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  skip_final_snapshot    = true
+}
+
+############################
+# LAMBDA PACKAGE
+############################
 
 data "archive_file" "zip" {
   type        = "zip"
@@ -26,30 +106,53 @@ data "archive_file" "zip" {
   output_path = "${path.module}/lambda.zip"
 }
 
-resource "aws_lambda_function" "hello" {
-  function_name = "jenkins-hello"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
-  filename      = data.archive_file.zip.output_path
+resource "aws_lambda_function" "api" {
+  function_name    = "user-api"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
+  filename         = data.archive_file.zip.output_path
   source_code_hash = data.archive_file.zip.output_base64sha256
+
+  vpc_config {
+    subnet_ids         = [aws_subnet.private1.id, aws_subnet.private2.id]
+    security_group_ids = [aws_security_group.lambda_sg.id]
+  }
+
+  environment {
+    variables = {
+      DB_HOST = aws_db_instance.app_db.address
+      DB_USER = "admin"
+      DB_PASS = "Admin12345!"
+      DB_NAME = "usersdb"
+    }
+  }
 }
 
+############################
+# API GATEWAY
+############################
+
 resource "aws_apigatewayv2_api" "api" {
-  name          = "jenkins-api"
+  name          = "user-api"
   protocol_type = "HTTP"
 }
 
 resource "aws_apigatewayv2_integration" "lambda" {
-  api_id             = aws_apigatewayv2_api.api.id
-  integration_type   = "AWS_PROXY"
-  integration_uri    = aws_lambda_function.hello.invoke_arn
-  integration_method = "POST"
+  api_id           = aws_apigatewayv2_api.api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.api.invoke_arn
 }
 
-resource "aws_apigatewayv2_route" "route" {
+resource "aws_apigatewayv2_route" "get_users" {
   api_id    = aws_apigatewayv2_api.api.id
-  route_key = "GET /hello"
+  route_key = "GET /users"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_route" "post_users" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "POST /users"
   target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
 }
 
@@ -62,6 +165,37 @@ resource "aws_apigatewayv2_stage" "stage" {
 resource "aws_lambda_permission" "api" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.hello.function_name
+  function_name = aws_lambda_function.api.function_name
   principal     = "apigateway.amazonaws.com"
+}
+
+############################
+# SNS + CLOUDWATCH ALARM
+############################
+
+resource "aws_sns_topic" "alerts" {
+  name = "rds-alerts"
+}
+
+resource "aws_sns_topic_subscription" "email" {
+  topic_arn = aws_sns_topic.alerts.arn
+  protocol  = "email"
+  endpoint  = "abbad.hassan@icommunix.com"
+}
+
+resource "aws_cloudwatch_metric_alarm" "rds_cpu" {
+  alarm_name          = "rds-high-cpu"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/RDS"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 80
+
+  dimensions = {
+    DBInstanceIdentifier = aws_db_instance.app_db.id
+  }
+
+  alarm_actions = [aws_sns_topic.alerts.arn]
 }
