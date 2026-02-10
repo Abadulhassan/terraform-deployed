7e58c8538b6012044fe1b0645910201a582f43c6
Abadulhassan <45556780+Abadulhassan@users.noreply.github.com>
2026-02-10 17:34:06 +0500

    Update index.js
    lambda committed

--- lambda/index.js
+++ lambda/index.js
@@ -1,6 +1,21 @@
+const mysql = require('mysql2/promise');
+
 exports.handler = async (event) => {
-return {
-statusCode: 200,
-body: JSON.stringify({ message: "Hello from Terraform 🚀" })
-};
+  const conn = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  });
+
+  if (event.requestContext.http.method === "GET") {
+    const [rows] = await conn.query("SELECT * FROM users");
+    return { statusCode: 200, body: JSON.stringify(rows) };
+  }
+
+  if (event.requestContext.http.method === "POST") {
+    const body = JSON.parse(event.body);
+    await conn.query("INSERT INTO users(name,email) VALUES (?,?)",[body.name, body.email]);
+    return { statusCode: 201, body: "User created" };
+  }
 };
